763b6195c87656ffd18e127b7728db1ab51d203f
Jennifer Hansson <jennifermhansson@gmail.com>
2025-12-09 13:48:05 +0100

    Added BASE_URL

--- src/api/useMealdb.ts
+++ src/api/useMealdb.ts
@@ -1,14 +1,15 @@
 // useMealdb.ts
 import { useEffect, useState } from "react";
 
+const BASE_URL = "https://www.themealdb.com/api/json/v1/1/filter.php?c=vegetarian";
+
+
 export function useMealdb() {
   const [meals, setMeals] = useState<any[]>([]);
 
   useEffect(() => {
     const getMeals = async () => {
-      const response = await fetch(
-        "https://www.themealdb.com/api/json/v1/1/filter.php?c=vegetarian"
-      );
+      const response = await fetch(BASE_URL);
       const parsed = await response.json();
       setMeals(parsed.meals.slice(0, 6) || []);
     };
