457d032f73a8f474b104a74bde8b43ef6a7cfd98
mirafl0res <wasp.answer5v@icloud.com>
2025-12-09 19:05:18 +0100

    update RecipeCards to display results from spoonacular search

--- src/components/RecipeCards.tsx
+++ src/components/RecipeCards.tsx
@@ -1,35 +1,40 @@
-import { useMealdb } from "../api/useMealdb";
-import "../App.css";
 import { useFavoritesStore } from "../store/favoritesStore";
+import type { Recipe } from "../types/api";
+import "../App.css";
 
 type RecipeCardsProps = {
+  recipes: Recipe[];
   filterIds?: string[];
 };
 
-function RecipeCards({ filterIds }: RecipeCardsProps) {
-  const meals = useMealdb();
-
+function RecipeCards({ recipes = [], filterIds }: RecipeCardsProps) {
   const favorites = useFavoritesStore((state) => state.favorites);
   const toggleFavorite = useFavoritesStore((state) => state.toggleFavorite);
 
-  const isFavorite = (id: string) => favorites.includes(id);
+  const isFavorite = (id: number) => favorites.includes(String(id));
+
+  const displayRecipes = filterIds
+    ? recipes.filter((recipe) => filterIds.includes(String(recipe.id)))
+    : recipes;
 
   return (
     <div className="recipe-grid">
-      {(filterIds ? meals.filter((meal) => filterIds.includes(String(meal.idMeal))) : meals).map((meal) => (
-        <div key={meal.idMeal} className="recipe-card">
+      {displayRecipes.map((recipe) => (
+        <div key={recipe.id} className="recipe-card">
           <div className="recipe-img-wrapper">
-            <img src={meal.strMealThumb} alt={meal.strMeal} />
+            <img src={recipe.image} alt={recipe.title} />
           </div>
 
           <div className="recipe-content">
-            <h2 className="recipe-title">{meal.strMeal}</h2>
+            <h2 className="recipe-title">{recipe.title}</h2>
 
             <button
-              className={`favorite-btn ${isFavorite(String(meal.idMeal)) ? "active" : ""}`}
-              onClick={() => toggleFavorite(String(meal.idMeal))}
+              className={`favorite-btn ${
+                isFavorite(recipe.id) ? "active" : ""
+              }`}
+              onClick={() => toggleFavorite(String(recipe.id))}
             >
-              {isFavorite(String(meal.idMeal)) ? "❤︎" : "♡"}
+              {isFavorite(recipe.id) ? "❤︎" : "♡"}
             </button>
           </div>
         </div>
