cff48e95393f9e98f7fcb3821881fb3a7692cf30
Jennifer Hansson <jennifermhansson@gmail.com>
2025-12-10 08:47:11 +0100

    Refactoring components (#20)
    * Added BASE_URL
    
    * minor refactoring in components for cleaner code and structure

--- src/components/RecipeCards.tsx
+++ src/components/RecipeCards.tsx
@@ -18,29 +18,27 @@ function RecipeCards({ recipes, filterIds }: RecipeCardsProps) {
     : recipes;
 
   return (
+
     <div className="recipe-grid">
-      {displayRecipes.map((recipe) => (
-        <div key={recipe.id} className="recipe-card">
-          <div className="recipe-img-wrapper">
-            <img src={recipe.image} alt={recipe.title} />
-          </div>
+        {displayRecipes.map((recipe) => (
+    <div key={recipe.id} className="recipe-card">
+      <div className="recipe-img-wrapper">
+        <img src={recipe.image} alt={recipe.title} />
+      </div>
 
-          <div className="recipe-content">
-            <h2 className="recipe-title">{recipe.title}</h2>
+      <div className="recipe-content">
+        <h2 className="recipe-title">{recipe.title}</h2>
 
-            <button
-              className={`favorite-btn ${
-                isFavorite(recipe.id) ? "active" : ""
-              }`}
-              onClick={() => toggleFavorite(String(recipe.id))}
-            >
-              {isFavorite(recipe.id) ? "❤︎" : "♡"}
-            </button>
-          </div>
-        </div>
-      ))}
+        <button
+          className={`favorite-btn ${isFavorite(recipe.id) ? "active" : ""}`}
+          onClick={() => toggleFavorite(String(recipe.id))}
+        >
+          {isFavorite(recipe.id) ? "❤︎" : "♡"}
+        </button>
+      </div>
     </div>
-  );
+  ))}
+</div>
+  )
 }
-
 export default RecipeCards;
