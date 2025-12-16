533336c82e0b8d454e4fadfa67eccda9fb9faa31
mirafl0res <wasp.answer5v@icloud.com>
2025-12-16 13:36:32 +0100

    implement basic toggle functionality in SearchModeToggle and add types

--- src/components/search/SearchModeToggle.tsx
+++ src/components/search/SearchModeToggle.tsx
@@ -1,10 +1,20 @@
-function SearchModeToggle() {
-  
+type SearchMode = "default" | "ingredients";
+type SearchModeToggleProps = {
+  onModeChange: (mode: SearchMode) => void;
+};
+
+function SearchModeToggle({ onModeChange }: SearchModeToggleProps) {
+  const handleClick = (mode: SearchMode) => {
+    onModeChange(mode);
+  };
+
   return (
     <>
       <div>
-        <button onClick={() => {}}>Search Anything</button>
-        <button onClick={() => {}}>What's In My Fridge?</button>
+        <button onClick={() => handleClick("default")}>Search Anything</button>
+        <button onClick={() => handleClick("ingredients")}>
+          What's In My Fridge?
+        </button>
       </div>
     </>
   );
