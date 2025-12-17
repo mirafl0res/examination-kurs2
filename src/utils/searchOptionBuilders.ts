494973f7f57fc1d11dd50a5b844e7f161cc676b7
mirafl0res <wasp.answer5v@icloud.com>
2025-12-17 11:09:54 +0100

    fix: add missing addRecipeInformation to search option builders

--- src/utils/searchOptionBuilders.ts
+++ src/utils/searchOptionBuilders.ts
@@ -10,15 +10,17 @@ export const formatFilters = (filters: Filters) => ({
 
 export const buildDefaultOptions = (query: string, filters: Filters): SearchOptions => ({
   query,
+  addRecipeInformation: true,
   number: DEFAULT_RECIPE_COUNT,
   ...formatFilters(filters),
 });
 
 export const buildIngredientsOptions = (includeIngredients: string, filters: Filters): SearchOptions => ({
   includeIngredients,
+  addRecipeInformation: true,
+  number: DEFAULT_RECIPE_COUNT,
   fillIngredients: true,
   sort: "min-missing-ingredients",
-  number: DEFAULT_RECIPE_COUNT,
   ...formatFilters(filters),
 });
 
