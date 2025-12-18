f673e5f2449a824e0a9c98a0fbb8310cd63fe864
mirafl0res <wasp.answer5v@icloud.com>
2025-12-18 10:24:52 +0100

    feat: implement state, add/remove logic, and pill display in IngredientInput

--- src/components/search/IngredientInput.tsx
+++ src/components/search/IngredientInput.tsx
@@ -1,22 +1,51 @@
+import { useState } from "react";
+import { PillGroup } from "./PillGroup";
+import { togglePrimitiveInArray } from "../../utils/toggleHelpers";
+
 function IngredientInput() {
-/**
-|--------------------------------------------------
- 1. State management
- 2. Input & Add Logic
- 3. Display pills
- 4. Search Trigger
- 5. Props
-   * 
-|--------------------------------------------------
-*/
+  const [value, setValue] = useState("");
+  const [ingredients, setIngredients] = useState<string[]>([]);
 
-// const [, set] = useState();
+  const handleAddIngredient = (e) => {
+    e.preventDefault();
+    const ingredient = (value ?? "").trim();
+    if (!ingredient) return;
+    if (ingredients.includes(ingredient)) return;
+    setIngredients((prev) => [...prev, ingredient]);
+    setValue("");
+  };
 
+  const handleRemoveIngredient = (ingredient) => {
+    setIngredients(prev => togglePrimitiveInArray(prev, ingredient))   
+  }
 
+  return (
+    <>
+      <h3>IngredientInput</h3>
 
-    return ( 
-        <><h3>IngredientInput</h3></>
-     );
+      <form onSubmit={handleAddIngredient}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Enter ingredients"
+        />
+        <button type="submit">Add</button>
+      </form>
+      <PillGroup options={ingredients} selected={[]} onToggle={handleRemoveIngredient}/>
+    </>
+  );
 }
 
 export default IngredientInput;
+
+/**
+|--------------------------------------------------
+1. State management
+2. Input & Add Logic
+3. Display pills
+4. Search Trigger
+5. Props
+ * 
+|--------------------------------------------------
+*/
