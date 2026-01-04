452a1934c7d2a36d6cf7887daa4b5c964ba4b86f
Jennifer Hansson <jennifermhansson@gmail.com>
2026-01-04 12:37:11 +0100

    changed accent colors, fixed logo, added hero section to homepage, refactored variables in all css files (#62)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import { Icon } from "./ui/icon"
+import { Icons } from "./ui/icons"
+
+function Footer() {
+  return (
+    <div className="footer">
+      © 2026 by Philip & Jennifer
+      <span className="footer-icon"><Icon icon={Icons.chefHat} size={13} /></span>
+    </div>
+  )
+}
+
+export default Footer
