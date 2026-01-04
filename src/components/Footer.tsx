e4158e461bcfeb0c8b683962c8e2733160f5046d
mirafl0res <wasp.answer5v@icloud.com>
2026-01-05 00:56:49 +0100

    fix: remove margin from .search-container, and add inline-styling to Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,17 @@
-import { Icon } from "./ui/icon"
-import { Icons } from "./ui/icons"
+import { Icon } from "./ui/icon";
+import { Icons } from "./ui/icons";
 
 function Footer() {
   return (
-    <div className="footer">
-      © 2026 by Philip & Jennifer
-      <span className="footer-icon"><Icon icon={Icons.chefHat} size={13} /></span>
+    <div style={{ paddingTop: "3.5rem" }}>
+      <div className="footer">
+        © 2026 by Philip & Jennifer
+        <span className="footer-icon">
+          <Icon icon={Icons.chefHat} size={13} />
+        </span>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
