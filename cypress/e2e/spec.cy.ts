e60c047f9e6026ae82f3780d2aa0b90951df7278
mirafl0res <wasp.answer5v@icloud.com>
2026-01-05 16:27:25 +0100

    test: refactor cypress tests and add button ids for selectors

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,48 +1,93 @@
 /// <reference types="cypress" />
 
-describe("recipe app test", () => {
-  it("searches for, reads and toggles favorite recipes(mock)", () => {
+const TEST_INGREDIENT = "pasta";
+const TEST_INTOLERANCE = "Egg";
+const TEST_DIET = "vegetarian";
+const TEST_MAX_TIME = "45";
+
+describe("Recipe App", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:5173/");
+  });
 
-    cy.get(".navbar").should("be.visible");
-    cy.get(".logo").should("contain.text", "Recipe finder");
+  describe("Navigation", () => {
+    it("should display navbar with logo", () => {
+      cy.get(".navbar").should("be.visible");
+      cy.get(".logo").should("contain.text", "Recipe finder");
+    });
+  });
 
-    cy.get("#search-ingredient-btn").should("be.visible").click();
+  describe("Search functionality", () => {
+    it("should allow searching with filters", () => {
+      cy.get("#search-ingredient-btn").click();
+      cy.get("#searchField").type(TEST_INGREDIENT);
 
-    cy.get("#searchField").should("be.visible").type("pasta");
-    cy.get("#filters-btn").should("be.visible").click();
-    cy.get("#filters-intolerances-btn").should("be.visible").click();
-    cy.get(".pill-group").should("be.visible").contains("Egg").click();
-    cy.get("#filters-diets-btn").should("be.visible").click();
-    cy.get(".pill-group").should("be.visible").contains("vegetarian").click();
+      cy.get("#filters-btn").click();
+      cy.get("#filters-intolerances-btn").click();
+      cy.get(".pill-group").contains(TEST_INTOLERANCE).click();
 
-    cy.get("#filters-extras-btn").should("be.visible").click();
-    cy.get(".dropdown-container")
-      .should("be.visible")
-      .contains("label", /Max time/)
-      .find("select")
-      .should("be.visible")
-      .select("45")
-      .should("have.value", "45");
+      cy.get("#filters-diets-btn").click();
+      cy.get(".pill-group").contains(TEST_DIET).click();
 
-    cy.get("#search-add-btn").click();
-    cy.get(".search-ingredients-btn").click();
+      cy.get("#filters-extras-btn").click();
+      cy.get(".dropdown-container")
+        .contains("label", /Max time/)
+        .find("select")
+        .select(TEST_MAX_TIME)
+        .should("have.value", TEST_MAX_TIME);
 
-    cy.get(".recipe-card").eq(0).should("be.visible").click();
-    cy.url().should("include", "/recipe");
-    cy.get("article").find("h1").should("be.visible");
-    cy.get(".favorite-btn").should("be.visible").click();
+      cy.get("#search-add-btn").click();
+      cy.get(".search-ingredients-btn").click();
 
-    cy.get(".nav-menu-btn").should("be.visible").click();
-    cy.get("a").contains("Favorite Recipes").click();
+      cy.get(".recipe-card").should("exist");
+    });
+  });
 
-    cy.url().should("include", "/favorites");
-    cy.get(".recipe-card").eq(0).should("be.visible").click();
-    cy.url().should("include", "/recipe");
-    cy.get(".favorite-btn").should("be.enabled").click();
+  describe("Recipe details", () => {
+    beforeEach(() => {
+      cy.get("#search-ingredient-btn").click();
+      cy.get("#searchField").type(TEST_INGREDIENT);
+      cy.get("#search-add-btn").click();
+      cy.get(".search-ingredients-btn").click();
+    });
 
-    cy.get(".back-button").should("be.visible").click();
-    cy.url().should("include", "/favorites");
-    cy.get("p").should("be.visible").contains("No favorites yet");
+    it("should navigate to recipe details page", () => {
+      cy.get(".recipe-card").eq(0).click();
+      cy.url().should("include", "/recipe");
+      cy.get("article h1").should("be.visible");
+    });
+  });
+
+  describe("Favorites", () => {
+    beforeEach(() => {
+      cy.get("#search-ingredient-btn").click();
+      cy.get("#searchField").type(TEST_INGREDIENT);
+      cy.get("#search-add-btn").click();
+      cy.get(".search-ingredients-btn").click();
+      cy.get(".recipe-card").eq(0).click();
+    });
+
+    it("should add recipe to favorites", () => {
+      cy.get(".favorite-btn").click();
+
+      cy.get(".nav-menu-btn").click();
+      cy.contains("a", "Favorite Recipes").click();
+
+      cy.url().should("include", "/favorites");
+      cy.get(".recipe-card").should("exist");
+    });
+
+    it("should remove recipe from favorites", () => {
+      cy.get(".favorite-btn").click();
+      cy.get(".nav-menu-btn").click();
+      cy.contains("a", "Favorite Recipes").click();
+
+      cy.get(".recipe-card").eq(0).click();
+      cy.get(".favorite-btn").click();
+
+      cy.get(".back-button").click();
+      cy.url().should("include", "/favorites");
+      cy.contains("p", "No favorites yet").should("be.visible");
+    });
   });
 });
